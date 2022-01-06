442b7e898809753dc576c4541d83c2008f5ed8cb
Marcos Verblud <marcosverblud@gmail.com>
2022-01-06 19:40:24 -0300

    Agrego component NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function NavBar (){
+
+    return(
+        <>
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Quienes Somos</a></li>
+                <li><a href="">Tienda</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        
+        </>
+
+    )
+
+}
